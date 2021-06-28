e2d63d86aa796edeb7533bcd20bb39c55b693b5a
Raydioscope <86481817+Raydioscope@users.noreply.github.com>
2021-06-29 00:23:13 +0530

    Add files via upload

--- IotSwitch.js
+++ IotSwitch.js
@@ -1,40 +1,39 @@
 var SwitchStatus = [];
 var res;
-const url = "https://script.google.com/macros/s/AKfycbyn2h7Fet7vkbThSxAHJWwBf_ODRIWLQPy9-hMMWc4TmihxyZ-ijxmqVVIIaMEH-S9V/exec"
+const url = "https://script.google.com/macros/s/AKfycbxYGc5VYOxQuwLfSsunfWln7hvIAn0PB0xV-7pZ-EXndanBsjTURKPwEOhOXcdphhnZ/exec"
 
-window.onload = function() {  
-    
-    getAPIData();    
-    
+window.onload = function() {      
+    getAPIData();        
 };
 
 
 function setStatus() 
 {
    
-   const switch1 = document.getElementById("switchin1");
-   switch1.checked = SwitchStatus[1];
-   const switch2 = document.getElementById("switchin2");
-   switch2.checked = SwitchStatus[2];
-   const switch3 = document.getElementById("switchin3");
-   switch3.checked = SwitchStatus[3];
-   const switch4 = document.getElementById("switchin4");
-   switch4.checked = SwitchStatus[4];
+   const switch1 = document.getElementById("Switch1");
+   switch1.checked = SwitchStatus[0];
+   const switch2 = document.getElementById("Switch2");
+   switch2.checked = SwitchStatus[1];
+   const switch3 = document.getElementById("Switch3");
+   switch3.checked = SwitchStatus[2];
+   const switch4 = document.getElementById("Switch4");
+   switch4.checked = SwitchStatus[3];
 
 }
 
 function setSwitchDataOnClick(switchnumber)
 {
         switchnumber = parseInt(switchnumber);
-        var switchID = "switchin" + switchnumber;
-        SwitchStatus[switchnumber] = document.getElementById(switchID).checked ;
+        var switchID = "Switch" + switchnumber;
+        SwitchStatus[switchnumber-1] = document.getElementById(switchID).checked ;
+        postAPIData(switchnumber, SwitchStatus[switchnumber-1]);
         setStatus(); 
 
 }
 
 function getAPIData()
 {   
-    
+      
     fetch(url)
     .then(d => d.json())
     .then(d =>{
@@ -46,9 +45,27 @@ function getAPIData()
      
 }
 
-function postAPIData()
+function postAPIData(switchID,state)
 {
-    
+
+    var UPswitch = new Object();
+    UPswitch.switchID = switchID;
+    UPswitch.state = state;
+   
+
+    fetch(url,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        //credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        //redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(UPswitch) // body data type must match "Content-Type" header
+      })
 
 }
 
